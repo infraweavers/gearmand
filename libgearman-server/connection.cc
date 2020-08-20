bf2bd2abf42c8dddaee0dabbca6fecf184e1cb84
Benjamin Davis <BenjaminDavis@sta.cwserverfarm.local>
2020-08-20 10:46:12 +0100

    Signed-off-by: Benjamin Davis <BenjaminDavis@sta.cwserverfarm.local>

--- libgearman-server/connection.cc
+++ libgearman-server/connection.cc
@@ -441,8 +441,7 @@ gearman_server_con_st * gearman_server_con_to_be_freed_next(gearman_server_threa
       GEARMAND_LIST_DEL(thread->to_be_freed, con, to_be_freed_);
       /*DEBUG LOGGING*/
           gearmand_log_info(GEARMAN_DEFAULT_LOG_PARAM,
-                       "connection.cc - line 441 - GEARMAND_LIST_DEL",
-                       job->job_handle, job->unique);
+                       "connection.cc - line 441 - GEARMAND_LIST_DEL");
         if (con->to_be_freed_list)
         {
           con->to_be_freed_list= false;
@@ -516,8 +515,7 @@ void gearman_server_con_io_remove(gearman_server_con_st *con)
       GEARMAND_LIST_DEL(con->thread->io, con, io_);
       /*DEBUG LOGGING*/
           gearmand_log_info(GEARMAN_DEFAULT_LOG_PARAM,
-                       "connection.cc - line 516 - GEARMAND_LIST_DEL",
-                       job->job_handle, job->unique);
+                       "connection.cc - line 516 - GEARMAND_LIST_DEL");
       con->io_list= false;
     }
     if ((lock_error= pthread_mutex_unlock(&con->thread->lock)))
@@ -603,8 +601,7 @@ void gearman_server_con_proc_remove(gearman_server_con_st *con)
       GEARMAND_LIST_DEL(con->thread->proc, con, proc_);
       /*DEBUG LOGGING*/
           gearmand_log_info(GEARMAN_DEFAULT_LOG_PARAM,
-                       "connection.cc - line 603 - GEARMAND_LIST_DEL",
-                       job->job_handle, job->unique);
+                       "connection.cc - line 603 - GEARMAND_LIST_DEL");
       con->proc_list= false;
     }
 
@@ -638,8 +635,7 @@ gearman_server_con_proc_next(gearman_server_thread_st *thread)
       GEARMAND_LIST_DEL(thread->proc, con, proc_);
       /*DEBUG LOGGING*/
           gearmand_log_info(GEARMAN_DEFAULT_LOG_PARAM,
-                       "connection.cc - line 638 - GEARMAND_LIST_DEL",
-                       job->job_handle, job->unique);
+                       "connection.cc - line 638 - GEARMAND_LIST_DEL");
       con->proc_list= false;
       if (!(con->proc_removed))
       {
@@ -674,8 +670,7 @@ static void _server_job_timeout(int fd, short event, void *arg)
 
   /*DEBUG LOGGING*/
   gearmand_log_info(GEARMAN_DEFAULT_LOG_PARAM,
-                       "connection.cc - line 659 - gearman_server_job_queue",
-                       job->job_handle, job->unique);
+                       "connection.cc - line 659 - gearman_server_job_queue");
   gearmand_error_t ret= gearman_server_job_queue(job);
 
   if (ret != GEARMAND_SUCCESS)
@@ -772,8 +767,7 @@ gearman_server_con_st *gearmand_ready(gearmand_connection_list_st *universal)
     GEARMAND_LIST_DEL(universal->ready_con, con, ready_);
     /*DEBUG LOGGING*/
           gearmand_log_info(GEARMAN_DEFAULT_LOG_PARAM,
-                       "connection.cc - line 772 - GEARMAND_LIST_DEL",
-                       job->job_handle, job->unique);
+                       "connection.cc - line 772 - GEARMAND_LIST_DEL");
     return con->root;
   }
 
